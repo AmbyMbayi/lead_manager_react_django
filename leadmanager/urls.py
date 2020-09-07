5d57cc1bd29db8a962cd356b50ff31633e802f9f
Moses Gitau <gitaumoses4@gmail.com>
2020-09-07 11:08:59 +0300

    Fix issues with static files and django whitenoise

--- leadmanager/urls.py
+++ leadmanager/urls.py
@@ -1,11 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('leads.urls')),
     path('', include('accounts.urls')),
-  
-    
 ]
